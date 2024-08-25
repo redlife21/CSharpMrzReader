92d4e252f8f3a2187d21131c7490980897dba92f
Emincan Ünal <unalemincan@gmail.com>
2024-08-25 17:05:01 +0300

    source code

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace MRZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MRZ.Properties.Resources.mrz1;
+            this.pictureBox1.Location = new System.Drawing.Point(702, 371);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 204);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(702, 174);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(281, 191);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(683, 530);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(702, 581);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(281, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(702, 608);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Kamera başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 548);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(274, 142);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(702, 637);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(281, 20);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "OCR başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(702, 662);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(281, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Dosya OCR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(701, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(282, 156);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(292, 548);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(388, 142);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 694);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,305 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Tesseract;
+using IronOcr;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace MRZ
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        Bitmap image;
+        Rectangle cropRect;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //KAMERA LİSTESİ ALMA
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in videoDevices)
+            {
+                comboBox1.Items.Add(filterInfo.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+
+            try
+            {
+                if (videoSource != null)
+                    videoSource.Stop();
+            }
+            catch
+            {
+                //işlendi işlenemedi
+            }
+
+
+            //KAMERA BAŞLATMA
+            videoSource = new VideoCaptureDevice(videoDevices[2].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            videoSource.Start();
+
+            timer1.Enabled = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (videoSource != null)
+                    videoSource.Stop();
+            }
+            catch 
+            {
+                //işlendi işlenemedi
+            }
+
+
+            //KAMERA BAŞLATMA
+            videoSource = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            videoSource.Start();
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //HER FRAME'İ PİCTUREBOX'A YANSITMA
+            image = (Bitmap)eventArgs.Frame.Clone();
+            image.RotateFlip(RotateFlipType.RotateNoneFlipX); //HARFLERDE DÖNÜYOR
+            pictureBox2.Image = image;
+
+            //kırpılcak alanı belirleme ve yansıtma
+            int startX = 50;
+            int startY = 50;
+            int width = 500;
+            int height = 150;
+            cropRect = new Rectangle(startX, startY, width, height);
+            pictureBox2.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ocr();
+        }
+        
+        public void ocr()
+        {
+            #region resim işleme
+            Bitmap croppedImage = CropImage(pictureBox2.Image as Bitmap, cropRect);
+            croppedImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            Bitmap bnw = GrayScaleFilter(croppedImage);
+            pictureBox3.Image = bnw;
+            #endregion
+
+            #region tesseract
+            //METNİ İŞLEME
+            if (image != null)
+            {
+                var ms = new MemoryStream();
+                bnw.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] fileBytes = ms.ToArray();
+                ms.Close();
+
+                using (var engine = new TesseractEngine(@"./tessdata", "kimlik", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromMemory(fileBytes))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            richTextBox2.Text = page.GetText();
+                            extractInformations(page.GetText());
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Önce bir görüntü yakalayın.");
+            }
+            #endregion
+            #region ironocr
+            //string filePath = "./image.png";
+            //bnw.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+            //var ocrInput = new OcrInput();
+            //ocrInput.LoadImage("./image.png");
+
+            //string imageText = new IronTesseract().Read(ocrInput).Text;
+            //richTextBox2.Text = imageText;
+            #endregion
+        }
+
+        void ocrFromFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string testImagePath = "";
+
+            openFileDialog.Filter = "Image Files (*.jpg; *.jpeg; *.png; *.gif; *.bmp)|*.jpg; *.jpeg; *.png; *.gif; *.bmp";
+            openFileDialog.Title = "Select an image file";
+
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                testImagePath = openFileDialog.FileName;
+            }
+
+
+            using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.TesseractAndLstm))
+            {
+                using (var img = Pix.LoadFromFile(testImagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        var text = page.GetText();
+                        richTextBox2.Text = page.GetText();
+                        MessageBox.Show(page.GetText());
+                    }
+                }
+            }
+        }
+
+        public Bitmap GrayScaleFilter(Bitmap image)
+        {
+            Bitmap grayScale = new Bitmap(image.Width, image.Height);
+
+            for (Int32 y = 0; y < grayScale.Height; y++)
+                for (Int32 x = 0; x < grayScale.Width; x++)
+                {
+                    Color c = image.GetPixel(x, y);
+
+                    Int32 gs = (Int32)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
+
+                    grayScale.SetPixel(x, y, Color.FromArgb(gs, gs, gs));
+                }
+            return grayScale;
+        }
+
+        void extractInformations(string mrzData)
+        {
+            if (mrzData.Length > 85)
+            {
+                string readedData;
+                while (mrzData.Length > 0 && mrzData[0] != 'I')
+                {
+                    mrzData = mrzData.Substring(1);
+                }
+
+                if (mrzData.Length >= 2 && mrzData.StartsWith("I<", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    timer1.Enabled = false;
+
+                    //MessageBox.Show("Baştan 2 harfi 'I<' karakterine eşittir.");
+                    readedData = "Belge Türü: Kimlik";
+                    if (mrzData.Substring(2, 3).Equals("TUR"))
+                    {
+                        readedData = readedData + "\nUyruk: Türk";
+                    }
+                    else
+                    {
+                        readedData = readedData + "\nUyruk: Okunamadı";
+                    }
+                    readedData = readedData + "\nSeri Numarası: " + mrzData.Substring(5, 9);
+                    readedData = readedData + "\nKimlik Numarası: " + mrzData.Substring(16, 11);
+
+                    var birth = "";
+                    var birthYear = mrzData.Substring(31, 2);
+                    var birthMonth = mrzData.Substring(33, 2);
+                    var birthDay = mrzData.Substring(35, 2);
+                    if (Convert.ToInt32(birthYear) > 24)
+                    {
+                        birth = birthDay + "/" + birthMonth + "/" + "19" + birthYear;
+                    }
+                    else
+                    {
+                        birth = birthDay + "/" + birthMonth + "/" + "20" + birthYear;
+                    }
+                    readedData = readedData + "\nDoğum tarihi: " + birth;
+
+                    if (mrzData.Substring(38, 1).Equals("M"))
+                    {
+                        readedData = readedData + "\nCinsiyet: Erkek";
+                    }
+                    else if (mrzData.Substring(38, 1).Equals("M"))
+                    {
+                        readedData = readedData + "\nCinsiyet: Kadın";
+                    }
+                    else
+                    {
+                        readedData = readedData + "\nCinsiyet: Okunamadı";
+                    }
+
+                    readedData = readedData + "\nSon geçerlilik tarihi: 20" + mrzData.Substring(39, 2) + "/" + mrzData.Substring(41, 2) + "/" + mrzData.Substring(43, 2);
+
+                    //readedData = readedData + "\n son satır: " + mrzData.Substring(62, 30);
+
+                    for (int i = 63; i < mrzData.Length; i++)
+                    {
+                        if (mrzData[i] == '<')
+                        {
+                            mrzData = mrzData.Remove(i, 1).Insert(i, " ");
+                        }
+                    }
+
+                    // Boşluklardan böler ve isim ve soyisim değişkenlerine atar
+                    string[] parcalar = mrzData.Substring(62).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    string soyisim = parcalar.Length > 0 ? parcalar[0] : "";
+                    string isim = parcalar.Length > 1 ? parcalar[1] : "";
+
+                    readedData += "\nİsim: " + isim;
+                    readedData += "\nSoyisim: " + soyisim;
+
+                    richTextBox3.Text = readedData;
+
+                    MessageBox.Show("Başarılı! Tamama tıklayınca okuma tekrar başlatılcak.");
+                    timer1.Enabled = true;
+                }
+                else
+                {
+                    //MessageBox.Show("Baştan 2 harfi 'I<' karakterine eşit değildir.");
+                }
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ocrFromFile();
+        }
+
+        private Bitmap CropImage(Bitmap source, Rectangle cropRect)
+        {
+            Bitmap croppedImage = new Bitmap(cropRect.Width, cropRect.Height);
+
+            using (Graphics g = Graphics.FromImage(croppedImage))
+            {
+                g.DrawImage(source, new Rectangle(0, 0, croppedImage.Width, croppedImage.Height),
+                    cropRect, GraphicsUnit.Pixel);
+            }
+
+            return croppedImage;
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen pen = new Pen(Color.Red, 2))
+            {
+                e.Graphics.DrawRectangle(pen, cropRect);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ocr();
+        }
+    }
+}
